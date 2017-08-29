a0e8a6a0339928edcb0f2c525a304449d8ff7315
ayubova <ayubova.yuliya@gmail.com>
2017-08-29 17:06:08 +0300

    add practice

---
+++ 01-unicode-total.js
@@ -0,0 +1,15 @@
+/*You'll be given a string, and have to return the total of all the unicode characters as an int.
+ Should be able to handle any characters sent at it.
+
+examples:
+
+uniTotal("a") == 97 uniTotal("aaa") == 291*/
+
+const uniTotal = (string) => {
+let result = 0;
+for(let i = 0; i < string.length; i++) {
+result += string.charCodeAt(i);
+};
+return result;
+}
+// total up dem unicodes!
