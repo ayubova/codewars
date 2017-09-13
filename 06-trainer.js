e7e8124546f13e22ed6bb76febbd7c832725fae5
ayubova <ayubova.yuliya@gmail.com>
2017-09-13 15:24:55 +0300

    add practice

---
+++ 06-trainer.js
@@ -0,0 +1,15 @@
+/* In this little assignment you are given a string of space separated numbers,
+and have to return the highest and lowest number.
+
+Example:
+
+highAndLow("1 2 3 4 5"); // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5" */
+
+function highAndLow(numbers){
+  const arrNumbers = numbers.split(' ');
+  const min = Math.min.apply(null, arrNumbers);
+  const max = Math.max.apply(null, arrNumbers);
+  return `${max} ${min}`;
+}
