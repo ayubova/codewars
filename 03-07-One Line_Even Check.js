7c8177d5d4f2649214b3f4cbb00c7a63b85dbf6b
ayubova <ayubova.yuliya@gmail.com>
2017-09-18 15:19:42 +0300

    add practice

---
+++ 03-07-One Line_Even Check.js
@@ -0,0 +1,5 @@
+// Create a function that accepts a number n as a parameter and returns true if the number is even
+// and false if the number is odd.The length of your code must be less than or equal to 19 characters.
+// You are not allowed to use %.
+
+const evenCheck=n=>!(n&1);
