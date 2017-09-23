909e2a3e7fee8e446cf4816b846c2d4b21a5450a
ayubova <ayubova.yuliya@gmail.com>
2017-09-23 12:42:51 +0300

    add practice

--- 12-07-Find the smallest integer in the array.js
+++ 12-07-Find the smallest integer in the array.js
@@ -5,3 +5,12 @@ class SmallestIntegerFinder {
     return Math.min.apply(this, args);
   }
 }
+
+
+function gps(s, x) {
+  const delta_distance = str => str.split('-')[1] - str.split('-')[0];
+  const speed = x.map(value => (3600 * delta_distance(value)) / s);
+  console.log(s, x, speed);
+  return Math.max.apply(null, speed);
+}
+
---
+++ 14-07-Padding.js
@@ -0,0 +1,11 @@
+/* In not more than 24 characters, write a function named p that pads it input to both sides of itself and returns the result.
+Instruction
+
+Code should not the more than 24 characters
+Character + and function repeat not allowed in the code.
+Only a single . character allowed
+Code must be on a single line
+${ character not allowed */
+
+
+const p=s=>s.concat(s,s)
