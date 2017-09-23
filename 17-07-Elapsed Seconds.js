a1804243ac4110860e5c6ed57475f271f868aa5f
ayubova <ayubova.yuliya@gmail.com>
2017-09-23 17:37:36 +0300

    add practice

---
+++ 17-07-Elapsed Seconds.js
@@ -0,0 +1,5 @@
+// Complete the function so that it returns the number of seconds that have elapsed between
+//  the start and end times given.
+
+const elapsedSeconds = (startDate, endDate) => (endDate - startDate) / 1000;
+
