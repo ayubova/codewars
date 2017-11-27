449c68177df0dd2f9075fac5f345e480d7d03f52
ayubova <ayubova.yuliya@gmail.com>
2017-11-27 20:02:17 +0300

    add practice

---
+++ 19-06-word-a10n-abbreviation.js
@@ -0,0 +1,6 @@
+const find = /[a-z]{4,}/gi;
+function replace(match) { return match[0] + (match.length - 2) + match[match.length - 1]; }
+
+function abbreviate(string) {
+  return string.replace(find, replace);
+}
---
+++ 20-06-create-phone-number.js
@@ -0,0 +1,2 @@
+const createPhoneNumber = numbers => '(' + numbers.slice(0,3).join('') + ')' + ' ' + numbers.slice(3,6).join('') + '-' + numbers.slice(6,10).join('')
+
