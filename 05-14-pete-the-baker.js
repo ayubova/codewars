107045f5ae37cc8463dcb563887ce587b9d5e32b
Yuliya Ayubova <ayubova.yuliya@gmail.com>
2017-12-15 20:50:59 +0300

    add practice

--- 05-14-pete-the-baker.js
+++ 05-14-pete-the-baker.js
@@ -1,4 +1,5 @@
 function cakes(recipe, available) {
+  
   const portions = Object.keys(recipe).reduce((acc, value) => [...acc, available[value] / recipe[value]], []);
   return portions.some(v => !v) ? 0 : Math.floor(Math.min.apply(null, portions));
 }
